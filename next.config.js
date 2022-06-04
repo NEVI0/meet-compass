ed6c1db70d03e83692acd468d8d764c8c323a9d2
NEVI0 <neviocostamagagnin@gmail.com>
2022-06-04 19:50:21 -0300

    feat: adding project configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	async redirects() {
+		return [
+			{
+				source: '/',
+				destination: '/home',
+				permanent: true
+			}
+		];
+	}
 }
 
 module.exports = nextConfig
