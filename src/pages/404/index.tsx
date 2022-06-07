3a9d4705cb455a603d4418967179a2493dc9e0f1
NEVI0 <neviocostamagagnin@gmail.com>
2022-06-06 23:46:59 -0300

    feat: adding not found page

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,12 +1,55 @@
+import React, { useEffect } from 'react';
+import Lottie from 'react-lottie';
+
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+
+import * as notFoundAnimation from '../../../public/assets/animations/not-found.json';
 import * as S from './styles';
 
 const NotFound: NextPage = () => {
+
+	const router = useRouter();
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			router.replace('/home');
+		}, 5000);
+
+		return () => clearTimeout(timer);
+	}, []);
+
 	return (
 		<S.NotFoundContainer>
-			<h1>NotFound Page</h1>
+			<main className="notfound">
+				<Lottie
+					width={ 500 }
+					height={ 250 }
+					isStopped={ false }
+					isPaused={ false }
+					options={{
+						loop: true,
+						autoplay: true, 
+						animationData: notFoundAnimation,
+						rendererSettings: {
+							preserveAspectRatio: 'xMidYMid slice'
+						}
+					}}
+				/>
+
+				<div>
+					<h1 className="notfound__title">
+						Ops... there is nothing here!
+					</h1>
+
+					<p className="notfound__message">
+						You are going to be redirect to home page in about 5 seconds.
+					</p>
+				</div>
+			</main>
 		</S.NotFoundContainer>
 	);
+
 }
 
 export default NotFound;
--- src/pages/404/styles.ts
+++ src/pages/404/styles.ts
@@ -1,5 +1,25 @@
 import styled from 'styled-components';
 
 export const NotFoundContainer = styled.div`
+	height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 
+	.notfound {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		row-gap: 3.25rem;
+		text-align: center;
+
+		&__title {
+			font-size: ${props => props.theme.font.h1Size};
+			margin-bottom: .25rem;
+		}
+
+		&__message {
+			color: ${props => props.theme.colors.textLight};
+		}
+	}
 `;
