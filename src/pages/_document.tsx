8835f7062afd02391baf067a2ba9dbcaaebaade5
NEVI0 <neviocostamagagnin@gmail.com>
2022-06-09 22:32:43 -0300

    fix: fixing document file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,13 +5,15 @@ const Document = () => {
 		<Html lang='en'>
 			<Head>
 				<meta charSet="utf-8" />
-				<link rel="shortcut icon" href="/images/favicon.png" />
+				{/* <link rel="shortcut icon" href="/images/favicon.png" /> */}
 
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" />
 				<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+
+				<script defer src="https://unpkg.com/peerjs@1.4.5/dist/peerjs.min.js" />
 			</Head>
-			<body className="theme-light">
+			<body>
 				<Main />
 				<NextScript />
 			</body>
