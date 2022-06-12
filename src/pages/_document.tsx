10b8608693c579278d6d7c10a8a19bbcbe51e3fc
NEVI0 <neviocostamagagnin@gmail.com>
2022-06-12 13:03:20 -0300

    fix: removing peer cdn link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,8 +10,6 @@ const Document = () => {
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" />
 				<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
-
-				<script defer src="https://unpkg.com/peerjs@1.4.5/dist/peerjs.min.js" />
 			</Head>
 			<body>
 				<Main />
