e288d09160b2952111c554fee3c57e40030ec5b2
NEVI0 <neviocostamagagnin@gmail.com>
2022-06-12 13:04:13 -0300

    feat: adding peer default connection

---
+++ src/utils/constants.ts
@@ -0,0 +1,16 @@
+const PEER_CONFIGS = {		  
+	iceServers: [
+		{
+			urls: "stun:numb.viagenie.ca",
+			username: "sultan1640@gmail.com",
+			credential: "98376683"
+		},
+		{
+			urls: "turn:numb.viagenie.ca",
+			username: "sultan1640@gmail.com",
+			credential: "98376683"
+		}
+	]
+}
+
+export { PEER_CONFIGS };
