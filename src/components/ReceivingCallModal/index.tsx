4b35fe2fc82549e657c2fbada28c06e48634947a
NEVI0 <neviocostamagagnin@gmail.com>
2022-06-14 22:14:37 -0300

    feat: setting other user data in the receiving call modal

--- src/components/ReceivingCallModal/index.tsx
+++ src/components/ReceivingCallModal/index.tsx
@@ -6,27 +6,27 @@ import * as S from './styles';
 
 const ReceivingCallModal: React.FC<{ visible: boolean; }> = ({ visible }) => {
 
-	const { acceptCall, rejectCallRequest } = useAppContext();
+	const { otherUserData, acceptMeetRequest, rejectMeetRequest } = useAppContext();
 
 	return (
 		<S.ReceivingCallModal visible={ visible }>
 			<div className="receivingcall">
 				<header className="receivingcall__header">
 					<h2 className="receivingcall__title">
-						Fernando is calling you
+						{ otherUserData.name } is calling you
 					</h2>
 
-					<button className="receivingcall__close" onClick={ rejectCallRequest }>
+					<button className="receivingcall__close" onClick={ rejectMeetRequest }>
 						<BiX />
 					</button>
 				</header>
 
 				<div className="receivingcall__content">
-					<button className="receivingcall__button receivingcall__button-default" onClick={ rejectCallRequest }>
+					<button className="receivingcall__button receivingcall__button-default" onClick={ rejectMeetRequest }>
 						Decline
 					</button>
 
-					<button className="receivingcall__button receivingcall__button-primary" onClick={ acceptCall }>
+					<button className="receivingcall__button receivingcall__button-primary" onClick={ acceptMeetRequest }>
 						Accept
 					</button>
 				</div>
