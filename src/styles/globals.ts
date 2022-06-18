66ca47195d06850b0cf5f70b178965cdf568fced
NEVI0 <neviocostamagagnin@gmail.com>
2022-06-18 19:02:24 -0300

    feat: removing chrome autofill value styles

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -67,6 +67,15 @@ export const GlobalStyles = createGlobalStyle`
 		margin: 0;
 	}
 
+	input:-webkit-autofill,
+	input:-webkit-autofill:hover, 
+	input:-webkit-autofill:focus, 
+	input:-webkit-autofill:active{
+		-webkit-box-shadow: 0 0 0px 1000px ${props => props.theme.colors.body} inset;
+		-webkit-text-fill-color: ${props => props.theme.colors.text};
+		transition: background-color 5000s ease-in-out 0s;
+	}
+
 	/* Firefox */
 	input[type=number] {
 		-moz-appearance: textfield;
