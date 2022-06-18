ac180ef83379833ad534e2e7347b2b44b13adf67
NEVI0 <neviocostamagagnin@gmail.com>
2022-06-18 18:56:48 -0300

    feat: adding responsiveness to 404 page

--- src/pages/404/index.page.tsx
+++ src/pages/404/index.page.tsx
@@ -4,13 +4,25 @@ import Lottie from 'react-lottie';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 
+import { useWindowBreakpoints } from '../../hooks';
 import { LOTTIE_OPTIONS } from '../../utils/constants';
+
 import * as notFoundAnimation from '../../../public/assets/animations/not-found.json';
 import * as S from './styles';
+
+const ANIMATION_DIMENSIONS = {
+	'xsm': { width: 300, height: 150 },
+	'sm': { width: 300, height: 150 },
+	'md': { width: 300, height: 150 },
+	'lg': { width: 500, height: 250 },
+	'xl': { width: 500, height: 250 },
+	'xxl': { width: 500, height: 250 }
+}
 
 const NotFound: NextPage = () => {
 
 	const router = useRouter();
+	const breakpoint = useWindowBreakpoints();
 
 	useEffect(() => {
 		const timer = setTimeout(() => {
@@ -24,11 +36,12 @@ const NotFound: NextPage = () => {
 		<S.NotFoundContainer>
 			<main className="notfound">
 				<Lottie
-					width={ 500 }
-					height={ 250 }
 					isStopped={ false }
 					isPaused={ false }
 					isClickToPauseDisabled={ true }
+					style={{ transition: '.3s' }}
+					width={ ANIMATION_DIMENSIONS[breakpoint].width }
+					height={ ANIMATION_DIMENSIONS[breakpoint].height }
 					options={{ ...LOTTIE_OPTIONS, animationData: notFoundAnimation }}
 				/>
 
