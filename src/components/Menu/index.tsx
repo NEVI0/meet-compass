e7a614124b46fe2e7c7c275f9af0503a1dfe1ced
NEVI0 <neviocostamagagnin@gmail.com>
2022-06-25 18:49:36 -0300

    feat: adding menu component

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { BiX } from 'react-icons/bi';
+
+import { IconButton, LanguageSwitch } from '..';
+
+import useMeetContext from '../../contexts/MeetContext';
+import * as S from './styles';
+
+interface MenuProps {
+	visible: boolean;
+	onClose: () => void;
+	children: any;
+}
+
+const Menu: React.FC<MenuProps> = ({ visible, onClose, children }) => {
+
+	const { userData } = useMeetContext();
+
+	return (
+		<S.Menu visible={ visible }>
+			<header className="menu__header">
+				<div className="menu__data">
+					<h2 className="menu__title">
+						{ userData.name }
+					</h2>
+
+					<h3 className="menu__subtitle">
+						{ userData.email }
+					</h3>
+				</div>
+
+				<IconButton
+					onClick={ onClose }
+					icon={ <BiX /> }
+				/>
+			</header>
+
+			<main className="menu__content">
+				{ children }
+			</main>
+
+			<footer className="menu__footer">
+				<span className="menu__language">
+					Language:
+				</span>
+
+				<LanguageSwitch />
+			</footer>
+		</S.Menu>
+	);
+
+}
+
+export default Menu;
