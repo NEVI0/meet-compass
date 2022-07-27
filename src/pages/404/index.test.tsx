218f7a2be8110a5d30ef3a1eae40842ef53a731a
NEVI0 <neviocostamagagnin@gmail.com>
2022-07-26 22:20:35 -0300

    fix: removing warnings and errors in unit tests from not found page

--- src/pages/404/index.test.tsx
+++ src/pages/404/index.test.tsx
@@ -1,10 +1,20 @@
 import NotFound, { NOT_FOUND_SECONDS_TO_REDIRECT } from './index.page';
 import { render } from '../../test/defaultSetup';
 
-jest.setTimeout(10000);
-const useRouter = jest.spyOn(require('next/router'), 'useRouter');
-
 describe('not found page tests', () => {
+	jest.setTimeout(10000);
+
+	const originalWarn = console.warn.bind(console.warn);
+	const useRouter = jest.spyOn(require('next/router'), 'useRouter');
+
+	beforeAll(() => {
+		console.warn = () => '';
+	});
+
+	afterAll(() => {
+		console.warn = originalWarn;
+	});
+
 	test('redirect user after 5 seconds', async () => {
 		const redirectionFunction = jest.fn();
 
