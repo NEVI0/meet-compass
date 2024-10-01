e86a6bdce8bb1e4fefa48ec0be34b33c5ae1a367
NEVI0 <neviocostamagagnin@gmail.com>
2024-09-30 22:04:44 -0300

    temp: moving old project to a separe folder (just for now)

---
+++ old/utils/functions.ts
@@ -0,0 +1,31 @@
+export const isEmpty = (object: any) => {
+    if (object === null || object === undefined) return true;
+    return Object.keys(object).length === 0;
+};
+
+export const getWindowDimensions = () => {
+    try {
+        const { innerWidth: width, innerHeight: height } = window;
+        return { width, height };
+    } catch (error) {
+        return { width: 0, height: 0 };
+    }
+};
+
+export const isLink = (text: string) => {
+    const linkRegex = new RegExp(
+        '^(https?:\\/\\/)?' +
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+            '((\\d{1,3}\\.){3}\\d{1,3}))' +
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+            '(\\?[;&a-z\\d%_.~+=-]*)?' +
+            '(\\#[-a-z\\d_]*)?$',
+        'i',
+    );
+
+    return !!linkRegex.test(text);
+};
+
+export const hasKeyWordInWarning = (message: string) => {
+    return message.includes('componentWillUpdate');
+};
