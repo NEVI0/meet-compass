66dbde6e94e1ea22c8a66ac7d8b4b96be26c6c7a
NEVI0 <neviocostamagagnin@gmail.com>
2024-10-31 20:48:41 -0300

    fix: fixing theme property usage

--- src/presentation/design/defaults/index.ts
+++ src/presentation/design/defaults/index.ts
@@ -74,7 +74,7 @@ export const DefaultStyles = createGlobalStyle`
 	input:-webkit-autofill:focus, 
 	input:-webkit-autofill:active{
 		-webkit-box-shadow: 0 0 0px 1000px ${props => props.theme.colors.body} inset;
-		-webkit-text-fill-color: ${props => props.theme.colors.text};
+		-webkit-text-fill-color: ${props => props.theme.colors.text.main};
 		transition: background-color 5000s ease-in-out 0s;
 	}
 
