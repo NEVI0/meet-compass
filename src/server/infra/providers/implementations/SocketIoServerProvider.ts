df1cf3ad8fee18b962f35f5977862e8732eadeb6
NEVI0 <neviocostamagagnin@gmail.com>
2025-03-17 22:05:36 -0300

    refactor: removing console.log

--- src/server/infra/providers/implementations/SocketIoServerProvider.ts
+++ src/server/infra/providers/implementations/SocketIoServerProvider.ts
@@ -1,13 +1,11 @@
 import { IoServer, SocketServer } from '@shared/domain/entities';
 import { SocketServerProviderAbstract } from '@server/domain/providers';
-import { console } from 'inspector';
 
 export class SocketIoServerProvider implements SocketServerProviderAbstract {
     public io: IoServer;
     public socket: SocketServer | undefined;
 
     constructor(server: IoServer) {
-        console.log({ server });
         this.io = server;
     }
 
